a577cd7abdc542846cbd3fa3785e42ddd38fe3d7
Nuno Coimbra <ncoimbra@gmail.com>
2020-04-24 09:29:09 +0100

    .

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject 
     {
-        public int Id { get; set; }
-        [Display(Name = "First name")]
-        public string FirstName { get; set; }
-        [Display(Name = "Last name")]
-        public string LastName { get; set; }
-        public string Description { get; set; }
-        [Display(Name = "Email Address")]
-        public string EmailAddress { get; set; }
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-        public bool IsStaff { get; set; }
+        [Required]                                                          public int    Id           { get; set; }
+        [StringLength(maximumLength:100, MinimumLength = 2)][DataType(DataType.Text)] [Required] [Display(Name = "First name")] public string FirstName    { get; set; }
+        [StringLength(maximumLength:100, MinimumLength = 2)][DataType(DataType.Text)] [Required] [Display(Name = "Last name")]  public string LastName     { get; set; }
+        [StringLength(maximumLength:500, MinimumLength = 10)][DataType(DataType.MultilineText)] [Required]                       public string Description  { get; set; }
+        [DataType(DataType.EmailAddress)] [Display(Name = "Email Address")] public string EmailAddress { get; set; }
+        [DataType(DataType.PhoneNumber)] [Display(Name = "Phone Number")]   public string PhoneNumber  { get; set; }
+        public                                                                     bool   IsStaff      { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var valList = new List<ValidationResult>();
+
+            if (!string.IsNullOrWhiteSpace(EmailAddress) &&
+                EmailAddress.EndsWith("TechnologyLiveConference.com", StringComparison.InvariantCultureIgnoreCase))
+            {
+                valList.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+            return valList;
+        }
     }
 }
